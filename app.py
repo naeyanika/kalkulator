9d0e0b83a194e7d0cd487301e5f221226bb4241a
Joey, K <40598758+naeyanika@users.noreply.github.com>
2024-12-06 14:58:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,8 @@ def calculate_rating(minor, moderate, major, fraud):
         (moderate >= 8) or
         (major == 0 and moderate >= 8 and moderate <= 15) or    # Kriteria 1
         (major == 1 and moderate >= 8 and moderate <= 12) or    # Kriteria 2
-        (major == 2 and moderate >= 8 and moderate <= 10)       # Kriteria 3
+        (major == 2 and moderate >= 8 and moderate <= 10) or    # Kriteria 3
+        (major == 2 and moderate < 8)
     ):
         return "MEDIUM"
     
