36ef76f2eab304f991b205266bc98c4152519c93
Joey, K <40598758+naeyanika@users.noreply.github.com>
2024-12-06 09:46:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ def calculate_rating(minor, moderate, major, fraud):
 
 # Streamlit untuk antarmuka
 st.title("Kalkulator Audit Rating")
+st.markdown('''refresh web ini jika rating audit issue nya tidak bejalan''')
 
 # Input jumlah temuan
 minor = st.number_input("Masukkan jumlah temuan Minor:", min_value=0, step=1)
